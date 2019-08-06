63640485a92dfd29efe1890c99050807f1c56012
Reed <JRReed@TRI.NET>
2019-08-06 13:13:08 -0700

    Init packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { ToastrModule } from "ngx-toastr";
+import { environment } from "../environments/environment";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SharedModule } from "./shared/shared.module";
+import { StoreModule } from "@ngrx/store";
+import { reducers, metaReducers } from "./store/reducers";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    SharedModule,
+    StoreDevtoolsModule.instrument({
+      maxAge: 100,
+      logOnly: environment.production
+    }),
+    ToastrModule.forRoot({
+      enableHtml: true
+    }),
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
